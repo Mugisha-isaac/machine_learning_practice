b78681ad6d501210f86c8f94560aa61f45b0dc70
Mugisha-isaac <mugishaisaac0508@gmail.com>
2025-09-18 21:11:11 +0200

    ft: linear classifier

---
+++ linear-classifier.py
@@ -0,0 +1,28 @@
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_openml
+import numpy as np
+from sklearn.linear_model import SGDClassifier
+
+mnist = fetch_openml('mnist_784', version='active', as_frame=False)
+mnist.keys()
+X,Y = mnist["data"], mnist["target"]
+some_digit = X[0]
+some_digit_image = some_digit.reshape(28,28)
+
+# plt.imshow(some_digit_image,cmap= mpl.cm.binary, interpolation='nearest')
+# plt.axis('off')
+# plt.show()
+
+Y= Y.astype(np.uint8)
+
+X_train,X_test,y_train,y_test = X[:60000],X[60000:],Y[:60000],Y[60000:]
+
+y_train_5 = (y_train == 5)
+y_test_5 = (y_test == 5)
+
+sgd_clf = SGDClassifier(random_state=42)
+sgd_clf.fit(X_train,y_train_5)
+
+sgd_clf.predict([some_digit])
