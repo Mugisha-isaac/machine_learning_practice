29b5918f7c91022f70495eb2d6d103191bccf793
Mugisha-isaac <mugishaisaac0508@gmail.com>
2025-09-18 21:58:35 +0200

    ft: add recall and precision

--- linear-classifier.py
+++ linear-classifier.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import fetch_openml
 import numpy as np
 from sklearn.linear_model import SGDClassifier
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score,cross_val_predict
+from sklearn.base import BaseEstimator
+from sklearn.metrics import confusion_matrix
 
 mnist = fetch_openml('mnist_784', version='active', as_frame=False)
 mnist.keys()
@@ -29,3 +31,18 @@ sgd_clf.fit(X_train,y_train_5)
 sgd_clf.predict([some_digit])
 
 cross_val_score(sgd_clf,X_train,y_train_5, cv=3, scoring='accuracy')
+
+# class Never5Classifier(BaseEstimator):
+#   def fit(self, X, y=None):
+#     pass
+#   def predict(self, X):
+#     return np.zeros((len(X),1), dtype=bool)
+
+
+# never_5_cls = Never5Classifier()
+# cross_val_score(never_5_cls,X_train,y_train_5, cv=3, scoring='accuracy')
+
+
+# using confusion matrix to evaluate the model
+y_train_pred = cross_val_predict(sgd_clf,X_train,y_train_5, cv=3)
+confusion_matrix(y_train_5,y_train_pred)
